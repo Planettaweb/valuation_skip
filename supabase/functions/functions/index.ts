1d2a6c3bc28e9cce98e9f845d864b2eaf6f4c172
Skip <skip@usecurling.com>
2026-03-15 21:51:38 +0000

    Refactor: Simplify app structure to client-first

--- supabase/functions/functions/index.ts
+++ supabase/functions/functions/index.ts
@@ -1,7 +1,7 @@
 import 'jsr:@supabase/functions-js/edge-runtime.d.ts'
 
-Deno.serve(async (req: Request) => {
-  return new Response(JSON.stringify({ message: 'Edge function running' }), {
+Deno.serve(async (req) => {
+  return new Response(JSON.stringify({ message: 'Function works!' }), {
     headers: { 'Content-Type': 'application/json' },
   })
 })
